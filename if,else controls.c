b64ff433f7459b9b9ac7bade56e58ffc46f73fde
Cosmas-pro <91739410+Cosmas-pro@users.noreply.github.com>
2021-11-03 09:41:04 +0300

    Create if,else controls.c

---
+++ if,else controls.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter value of a");
+    scanf("%d",&a);
+    if(a>20)
+    {
+        printf("n is greater than 5");
+    }
+    else
+    {
+        printf("n is not");
+    }
+    return 0;
+}
